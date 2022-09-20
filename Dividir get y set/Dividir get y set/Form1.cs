17102e0ac59bc3b59950c0a3d70b8a65f1e31611
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:36:35 -0500

    Add files via upload

---
+++ Dividir get y set/Dividir get y set/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Dividir_get_y_set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dividirclass divi = new dividirclass();
+
+            divi.N11 = int.Parse(textBox1.Text);
+            divi.N21 = int.Parse(textBox2.Text);
+
+         
+
+            textBox3.Text = divi.divicion().ToString();
+        }
+    }
+}
---
+++ Dividir get y set/Dividir get y set/divi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dividir_get_y_set
+{
+    internal class dividirclass
+    {
+        
+        private double N1;
+        private double N2;
+
+        public double N11 { get => N1; set => N1 = value; }
+        public double N21 { get => N2; set => N2 = value; }
+
+      
+        public double divicion()
+        {
+            return (N1 / N2);
+        }
+    }
+}
